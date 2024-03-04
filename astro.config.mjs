d199a059889c62efa0561b57dc3a5c6d650e5a14
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2024-03-05 10:53:05 +1100

    chore: add 404, revert to blue

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@ import { loadEnv } from 'vite';
 
 import { defineConfig } from 'astro/config';
 import { sanityIntegration } from '@sanity/astro';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-const { SANITY_PROJECT_ID, SANITY_DATASET } = loadEnv(
+const { SANITY_STUDIO_PROJECT_ID, SANITY_STUDIO_DATASET } = loadEnv(
   process.env.NODE_ENV,
   process.cwd(),
   ''
@@ -16,12 +15,11 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [
     sanityIntegration({
-      projectId: SANITY_PROJECT_ID,
-      dataset: SANITY_DATASET,
+      projectId: SANITY_STUDIO_PROJECT_ID,
+      dataset: SANITY_STUDIO_DATASET,
       apiVersion: '2024-04-03',
       useCdn: false,
     }),
-    react(),
     tailwind(),
   ],
 });
--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const PAGE_TITLE = 'Portfolio for Cian Dolphin';
+export const PAGE_TITLE = 'Portfolio | Cian Dolphin';
 export const PAGE_DESCRIPTION =
   'Portfolio for Cian Dolphin, a software engineer based in Dublin, Ireland.';
